2b51bf71b91b274bf3b532cd6f6dacddc006607e
Nadrieril <nadrieril@gmail.com>
2014-11-25 10:20:38 +0100

    Scratchpad now uses view instead of greedyView

--- lib/Scratchpad.hs
+++ lib/Scratchpad.hs
@@ -3,7 +3,7 @@ module Scratchpad (scratchpadConfig, toggleScratchpad) where
 import XMonad
 
 import Control.Monad (liftM2, when)
-import qualified XMonad.StackSet as W (current, greedyView, hidden, shift, stack, tag, workspace, workspaces)
+import qualified XMonad.StackSet as W (current, view, hidden, shift, stack, tag, workspace, workspaces)
 import Data.Maybe (isNothing)
 import Data.List (find)
 import XMonad.Util.Run (runInTerm)
@@ -21,14 +21,14 @@ scratchpadConfig conf = conf
         composeAll [className =? c --> viewShift scratchpadName | c <- classes]
     }
     where classes = ["Gnome-terminal"]
-          viewShift = doF . liftM2 (.) W.greedyView W.shift
+          viewShift = doF . liftM2 (.) W.view W.shift
 
 
 toggleScratchpad = do
     ws <- gets windowset
     let crnt = W.workspace $ W.current ws
     let scratchpad = find ((== scratchpadName) . W.tag) (W.workspaces ws)
-    let switch = windows . W.greedyView
+    let switch = windows . W.view
     case () of
         () | W.tag crnt == scratchpadName -> switch $ W.tag $ head $ W.hidden ws
            | isNothing (W.stack =<< scratchpad) -> runInTerm "" "$SHELL"
