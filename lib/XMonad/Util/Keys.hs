38e8a7dc792e3e8d850839c23b561b48e6473dd5
Nadrieril <nadrieril@gmail.com>
2015-01-23 19:20:38 +0100

    Fixed copy/paste fail

--- lib/XMonad/Util/Keys.hs
+++ lib/XMonad/Util/Keys.hs
@@ -7,7 +7,7 @@ import XMonad.Actions.DynamicWorkspaces (withNthWorkspace)
 import qualified Data.Map
 import Control.Monad (liftM2)
 ------------------------------------------------------
-azertyKeys = mkKeys [0x26,0xe9,0x22,0x27,0x28,0x2d,0xe8,0x5f,0xe7,0xe0]
+azertyKeys = mkKeys ([0x26,0xe9,0x22,0x27,0x28,0x2d,0xe8,0x5f,0xe7,0xe0] :: [KeySym])
 
 numpadKeys = mkKeys
     [ xK_KP_End,  xK_KP_Down,  xK_KP_Page_Down -- 1, 2, 3
@@ -16,8 +16,7 @@ numpadKeys = mkKeys
     , xK_KP_Insert] -- 0
 
 
-mkKeys l (XConfig {modMask = modm}) = Data.Map.fromList $
-    ((modm, xK_semicolon), sendMessage (IncMasterN (-1))):
+mkKeys l (XConfig {modMask = modm}) = Data.Map.fromList
     [((m .|. modm, k), withNthWorkspace f i)
         | (i, k) <- zip [0..] l,
           (f, m) <- [(W.greedyView, 0), (liftM2 (.) W.view W.shift, shiftMask)]]
