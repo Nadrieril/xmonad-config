8dc114352b49717dc107dc5c1fe18bc66dfee171
Nadrieril <nadrieril@gmail.com>
2015-01-25 21:59:25 +0100

    Added generic topic that keeps atom in the master area

--- lib/Config/Topics.hs
+++ lib/Config/Topics.hs
@@ -2,13 +2,17 @@
 module Config.Topics (topicConfig, layout) where
 
 import XMonad
+import qualified XMonad.StackSet as W
 
 import XMonad.Actions.SpawnOn (spawnOn)
+import XMonad.Hooks.ManageHelpers (isDialog)
 
 import XMonad.Layout.NoBorders (smartBorders, noBorders)
 import XMonad.Layout.Tabbed (simpleTabbed)
 import XMonad.Layout.Reflect (reflectVert)
 -- import XMonad.Layout.Maximize (maximize)
+
+import Control.Applicative ((<$>))
 ------------------------------------------------------
 -- Custom libs
 import XMonad.Actions.DynamicTopicSpace (Topic(..), fromList, defaultTopic)
@@ -16,6 +20,13 @@ import XMonad.Hooks.ManageNext (queryFromClasses)
 import Config.Common
 ------------------------------------------------------
 topic = defaultTopic
+devTopic = topic {
+          topicLayout = Just $ Layout $ topbar ||| full
+        , topicHook = (not <$> (isDialog <||> isAtom) --> doF avoidMaster)
+                <+> (isAtom --> doF W.swapMaster)
+    }
+    where isAtom = queryFromClasses ["Atom"]
+
 
 topicConfig = fromList $
     [ ("web", topic {
@@ -40,26 +51,24 @@ topicConfig = fromList $
         topicWindows = queryFromClasses ["Eclipse"]
     })
     ] ++ map projectTopic
-        [ ("xm", topic {
+        [ ("xm", devTopic {
             topicDir = "xmonad"
         })
-        , ("b-a", topic {
+        , ("b-a", devTopic {
             topicDir = "bars-angular",
-            topicAction = const $ spawnLocalShellCmd "grunt serve",
-            topicLayout = Just $ Layout $ topbar ||| full
+            topicAction = const $ spawnLocalShellCmd "grunt serve"
         })
-        , ("b-d", topic {
+        , ("b-d", devTopic {
             topicDir = "bars-django",
             topicAction = const $ do
-                spawnLocalTerminal "ssh -t srv@nadrieril 'cd /srv/bars/bars-django; $SHELL'"
                 spawnLocalShell
-                spawnLocalShellCmd "python manage.py runserver_plus",
-            topicLayout = Just $ Layout $ topbar ||| full
+                spawnLocalTerminal "ssh -t srv@nadrieril 'cd /srv/bars/bars-django; $SHELL'"
+                spawnLocalShellCmd "python manage.py runserver_plus"
         })
-        , ("psc", topic {
+        , ("psc", devTopic {
             topicDir = "PSC"
         })
-        , ("24h", topic {
+        , ("24h", devTopic {
             topicDir = "24hnatation"
         })
         ] ++
@@ -92,20 +101,27 @@ topicConfig = fromList $
     [ ([i], topic) | i <- ['0'..'5'] ]
 
     where
-        projectTopic (w, t@Topic{topicDir = dir, topicAction = action}) = ("dev/"++w, t {
-                topicDir = "$HOME/projects/" ++ dir,
-                topicAction = \wk -> runOnByClass ("atom ~/projects/" ++ dir) classes wk >> action wk
+        projectTopic (w, t@Topic{topicDir = dir, topicAction = action}) =
+            ("dev/"++w, t {
+                  topicDir = "$HOME/projects/" ++ dir
+                , topicAction = \wk -> do
+                    runOnByClass ("atom ~/projects/" ++ dir) classes wk
+                    action wk
             })
             where classes = ["Atom"]
 
-        full = noBorders simpleTabbed
-        tiled = smartBorders (Tall 1 (3/100) (1/2))
-        topbar = smartBorders (reflectVert $ Mirror $ Tall 1 (3/100) (93/100))
-        doubletiled = smartBorders (Tall 2 (3/100) (1/2))
-        -- accordion = smartBorders (Mirror (Tall 0 (3/100) (1/2)))
 
+
+full = noBorders simpleTabbed
+tiled = smartBorders (Tall 1 (3/100) (1/2))
+topbar = smartBorders (reflectVert $ Mirror $ Tall 1 (3/100) (93/100))
+doubletiled = smartBorders (Tall 2 (3/100) (1/2))
+-- accordion = smartBorders (Mirror (Tall 0 (3/100) (1/2)))
 
 layout = full ||| tiled
-    where
-        full = noBorders simpleTabbed
-        tiled = smartBorders (Tall 1 (3/100) (1/2))
+
+
+avoidMaster :: W.StackSet i l a s sd -> W.StackSet i l a s sd
+avoidMaster = W.modify' $ \c -> case c of
+     W.Stack t [] (r:rs) -> W.Stack t [r] rs
+     otherwise           -> c
