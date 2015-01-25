e07545e5b8cfbacc6fe99477bb6208afb5acc23b
Nadrieril <nadrieril@gmail.com>
2015-01-25 03:40:29 +0100

    Removed useless DTS prefix from qualified import in Config.Topics

--- lib/Config/Topics.hs
+++ lib/Config/Topics.hs
@@ -6,95 +6,94 @@ import XMonad
 import XMonad.Actions.SpawnOn (spawnOn)
 
 import XMonad.Layout.NoBorders (smartBorders, noBorders)
-import XMonad.Layout.PerWorkspace (onWorkspaces)
 import XMonad.Layout.Tabbed (simpleTabbed)
 -- import XMonad.Layout.Maximize (maximize)
 ------------------------------------------------------
 -- Custom libs
-import qualified XMonad.Actions.DynamicTopicSpace as DTS
+import XMonad.Actions.DynamicTopicSpace (Topic(..), fromList, defaultTopic)
 import XMonad.Hooks.ManageNext (queryFromClasses)
 import Config.Common
 ------------------------------------------------------
-topic = DTS.defaultTopic
+topic = defaultTopic
 
-topicConfig = DTS.fromList $
+topicConfig = fromList $
     [ ("web", topic {
-        DTS.topicAction = flip spawnOn "google-chrome",
-        DTS.topicWindows = queryFromClasses ["Firefox","Google-chrome","Chromium","Chromium-browser"]
+        topicAction = flip spawnOn "google-chrome",
+        topicWindows = queryFromClasses ["Firefox","Google-chrome","Chromium","Chromium-browser"]
     })
     , ("main", topic)
     , ("game", topic)
     , ("video", topic {
-        DTS.topicDir = "$HOME/Videos",
-        DTS.topicAction = const spawnFilemanager,
-        DTS.topicWindows = queryFromClasses ["Vlc"]
+        topicDir = "$HOME/Videos",
+        topicAction = const spawnFilemanager,
+        topicWindows = queryFromClasses ["Vlc"]
     })
     ] ++
 
     [ ("dev", topic {
-        DTS.topicDir = "$HOME/projects"
+        topicDir = "$HOME/projects"
     })
     , ("dev/java", topic {
-        DTS.topicDir = "$HOME/projects/java",
-        DTS.topicAction = flip spawnOn "eclipse",
-        DTS.topicWindows = queryFromClasses ["Eclipse"]
+        topicDir = "$HOME/projects/java",
+        topicAction = flip spawnOn "eclipse",
+        topicWindows = queryFromClasses ["Eclipse"]
     })
     ] ++ map projectTopic
         [ ("xm", topic {
-            DTS.topicDir = "xmonad"
+            topicDir = "xmonad"
         })
         , ("b-a", topic {
-            DTS.topicDir = "bars-angular",
-            DTS.topicAction = const $ spawnLocalShellCmd "grunt serve",
-            DTS.topicLayout = Just $ Layout $ topbar ||| full
+            topicDir = "bars-angular",
+            topicAction = const $ spawnLocalShellCmd "grunt serve",
+            topicLayout = Just $ Layout $ topbar ||| full
         })
         , ("b-d", topic {
-            DTS.topicDir = "bars-django",
-            DTS.topicAction = const $ do
+            topicDir = "bars-django",
+            topicAction = const $ do
                 spawnLocalShellCmd "python manage.py runserver_plus"
                 spawnLocalShellCmd "ssh -t srv@nadrieril 'cd /srv/bars/bars-django; $SHELL'"
                 spawnLocalShell,
-            DTS.topicLayout = Just $ Layout $ topbar2 ||| full
+            topicLayout = Just $ Layout $ topbar2 ||| full
         })
         , ("psc", topic {
-            DTS.topicDir = "PSC"
+            topicDir = "PSC"
         })
         , ("24h", topic {
-            DTS.topicDir = "24hnatation"
+            topicDir = "24hnatation"
         })
         ] ++
 
     [ ("backup", topic {
-        DTS.topicAction = flip spawnOn "grsync"
+        topicAction = flip spawnOn "grsync"
     })
     , ("mail", topic {
-        DTS.topicAction = flip spawnOn "icedove"
+        topicAction = flip spawnOn "icedove"
     })
     , ("git", topic {
-        DTS.topicAction = flip spawnOn "smartgithg",
-        DTS.topicWindows = queryFromClasses ["SmartGit/Hg"]
+        topicAction = flip spawnOn "smartgithg",
+        topicWindows = queryFromClasses ["SmartGit/Hg"]
     })
     , ("irc", topic {
-        DTS.topicAction = flip spawnOn "quasselclient"
-        -- DTS.topicWindows = queryFromClasses ["quasselclient"]
+        topicAction = flip spawnOn "quasselclient"
+        -- topicWindows = queryFromClasses ["quasselclient"]
         --     <||> isInProperty "_NET_WM_NAME" "Quassel IRC"
         --     <||> isInProperty "WM_COMMAND" "quasselclient"
     })
     , ("music", topic {
-        DTS.topicDir = "$HOME/Music",
-        DTS.topicAction = flip spawnOn "rhythmbox",
-        DTS.topicWindows = queryFromClasses ["Rhythmbox", "ario"]
+        topicDir = "$HOME/Music",
+        topicAction = flip spawnOn "rhythmbox",
+        topicWindows = queryFromClasses ["Rhythmbox", "ario"]
     })
     , ("term", topic {
-        DTS.topicAction = const spawnLocalShell,
-        DTS.topicLayout = Just $ Layout $ doubletiled ||| full ||| tiled
+        topicAction = const spawnLocalShell,
+        topicLayout = Just $ Layout $ doubletiled ||| full ||| tiled
     })] ++
     [ ([i], topic) | i <- ['0'..'5'] ]
 
     where
-        projectTopic (w, t@DTS.Topic{DTS.topicDir = dir, DTS.topicAction = action}) = ("dev/"++w, t {
-                DTS.topicDir = "$HOME/projects/" ++ dir,
-                DTS.topicAction = \wk -> runOnByClass ("atom ~/projects/" ++ dir) classes wk >> action wk
+        projectTopic (w, t@Topic{topicDir = dir, topicAction = action}) = ("dev/"++w, t {
+                topicDir = "$HOME/projects/" ++ dir,
+                topicAction = \wk -> runOnByClass ("atom ~/projects/" ++ dir) classes wk >> action wk
             })
             where classes = ["Atom"]
 
