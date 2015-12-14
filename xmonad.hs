82e0071c32103dcfbe17c61358f61ea554392bca
Nadrieril <nadrieril@gmail.com>
2015-12-14 02:31:37 +0100

    Improve applet display on multi-head

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,8 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts, ParallelListComp #-}
 import XMonad
+import qualified XMonad.StackSet as W
 import XMonad.Hooks.DynamicLog
+import XMonad.Util.WorkspaceCompare (getSortByIndex)
 
 import XMonad.Config.Xfce (xfceConfig)
 
@@ -11,6 +13,9 @@ import XMonad.Hooks.ManageDocks (docksEventHook)
 import XMonad.Actions.SpawnOn (manageSpawn)
 import XMonad.Layout.Maximize (maximize)
 
+import GHC.Exts (sortWith)
+import qualified Data.Map
+import Data.Maybe (isJust)
 ------------------------------------------------------
 -- Custom libs
 import XMonad.Hooks.LogApplet as LogApplet
@@ -48,7 +53,7 @@ main = do
             , manageHook xfceConfig
             , manageHook' ]
         , handleEventHook    = handleEventHook xfceConfig <+> docksEventHook
-        , logHook            = logHook xfceConfig <+> dynamicLogWithPP (prettyPrinter applet)
+        , logHook            = logHook xfceConfig <+> appletlogHook applet
 }
 
 
@@ -66,17 +71,27 @@ manageHook' = composeAll $
         _floating  = ["Xmessage","Nm-connection-editor"]
         _ignored = ["desktop", "desktop_window", "notify-osd", "xfce4-notifyd", "Xfce4-notifyd", "stalonetray", "trayer"]
 
-prettyPrinter :: LogApplet.AppletPipe -> PP
-prettyPrinter applet = defaultPP
-    { ppOutput   = LogApplet.outputApplet applet . wrap "<b>" "</b>"
-    , ppTitle    = const ""
-    , ppCurrent  = wrap "[" "]" . pangoColor "green" . pangoSanitize
-    , ppVisible  = wrap "(" ")" . pangoSanitize
-    , ppHidden   = pangoColor "grey" . pangoSanitize
-    , ppUrgent   = pangoColor "red"
-    , ppLayout   = const ""
-    , ppSep      = " "
-    }
+appletlogHook :: AppletPipe -> X ()
+appletlogHook applet = do
+  ws_sort <- getSortByIndex
+  workspaces <- gets (map W.tag . ws_sort . W.workspaces . windowset)
+  let workspaces_map = Data.Map.fromList [(w, show i ++ "." ++ w) | i <- [1::Integer ..] | w <- workspaces]
+  let wtag = (Data.Map.!) workspaces_map . W.tag
+
+  current <- gets (W.current . windowset)
+  screens <- gets (W.screens . windowset)
+  let sorted_screens = sortWith W.screen screens
+  let logscreens = flip map sorted_screens $ \s ->
+        let name = pangoSanitize $ wtag $ W.workspace s in
+        if W.screen s == W.screen current then pangoColor "green" name else name
+
+  hidden <- gets (W.hidden . windowset)
+  let loghidden = flip map hidden $ \w ->
+        let name = pangoSanitize $ wtag w in
+        if isJust (W.stack w) then name else pangoColor "grey" name
+
+  let out = wrap "[" "]" (unwords logscreens) ++ " " ++ unwords loghidden
+  io $ LogApplet.outputApplet applet $ wrap "<b>" "</b>" out
 
 
 pangoColor :: String -> String -> String
